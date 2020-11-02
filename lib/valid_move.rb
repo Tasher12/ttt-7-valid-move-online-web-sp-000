7cb4ab1d23ab1a13f68b0b6697a7dce02ca88d27
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-11-02 15:29:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 
 def valid_move?(position_taken, index)
-  if position_taken?.between?(0,8) && position_taken? == false 
+  if position_taken.between?(0,8) && position_taken? == false 
     return true
-  elsif position_taken? == false || nil 
+  elsif position_taken == false || nil 
     return false
 end
 
