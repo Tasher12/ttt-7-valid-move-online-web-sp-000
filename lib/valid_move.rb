b22502142f1b41a1c788e1795ffe39731d787b48
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-11-02 15:15:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,18 @@
 # code your #valid_move? method here
 
+def valid_move?.between(0,8)
+  position_taken?
+end
+
+
+
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index);
-  if valid_move?.between?(0,8)
   if board[index] == "" || board[index] == " " || board[index] == nil 
     return false 
   elsif board[index] == "X" || board[index] == "O"
     return true 
   end 
  end
-end
+
